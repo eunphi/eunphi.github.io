b6035d93cdda50742d87770d5e781361c3dcd9d8
eunphi <midnightrumble1337@gmail.com>
2021-09-05 18:17:14 +0700

    Updates

---
+++ component---src-pages-404-js-7e903b4be07a5b9a9ad7.js
@@ -0,0 +1,2 @@
+(self.webpackChunkeunphi=self.webpackChunkeunphi||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t),n.d(t,{default:function(){return c}});var a=n(7294),l=n(7349),u=n(1633);function c(){return a.createElement(l.Z,null,a.createElement(u.Z,{data:{title:"Not Found :(",description:"404 Not Found."}}),a.createElement("div",{className:"px-2 text-lg"},a.createElement("h1",{className:"text-4xl mb-10"},"404 :("),a.createElement("p",null,"Halaman yang kamu cari nggak ketemu nih...")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7e903b4be07a5b9a9ad7.js.map
